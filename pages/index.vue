b54edde0867c8ae9f1f99ebe0e40f71d02f6aaf6
Maulana Sopian <maulana@antmediahost.com>
2025-12-01 16:03:03 +0700

    :tada: init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  tes
+  <div class="p-10">
+    <h1 class="text-4xl font-bold text-blue-500">
+      Tailwind v4 + Vite Plugin OK!
+    </h1>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
